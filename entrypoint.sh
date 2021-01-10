34b41fa15f7a9bd5ef26882691aac31df971c97b
whitebox <wbadmin@whitebox01.pipca.unisinos.br>
2021-01-10 20:18:29 -0300

    Corrected entrypoint to send SIGTERM to amf process

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,34 @@
 sed -i "s/{{AMF_IP}}/$AMF_IP/" ../config/amfcfg.conf
 sed -i "s/{{NRF_URI}}/$NRF_URI/" ../config/amfcfg.conf
 
-./amf -amfcfg ../config/amfcfg.conf
+
+prep_term()
+{
+    unset term_child_pid
+    unset term_kill_needed
+    trap 'handle_term' TERM INT
+}
+
+handle_term()
+{
+    if [ "${term_child_pid}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null
+    else
+        term_kill_needed="yes"
+    fi
+}
+
+wait_term()
+{
+    term_child_pid=$!
+    if [ "${term_kill_needed}" ]; then
+        kill -TERM "${term_child_pid}" 2>/dev/null
+    fi
+    wait ${term_child_pid} 2>/dev/null
+    trap - TERM INT
+    wait ${term_child_pid} 2>/dev/null
+}
+
+prep_term
+./amf -amfcfg ../config/amfcfg.conf &
+wait_term
